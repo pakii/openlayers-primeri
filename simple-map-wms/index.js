f09219b49cf03eecbcf012828418a7299670feb4
Aleksandar Pavlovic <apavlovic@Aleksandars-MacBook-Pro.local>
2022-07-05 04:49:04 +0200

    change wms in 2nd example

--- simple-map-wms/index.js
+++ simple-map-wms/index.js
@@ -1,20 +1,25 @@
 
-const map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
+const layers = [
+    new ol.layer.Tile({
+        source: new ol.source.OSM(),
+    }),
+    new ol.layer.Tile({
+        title: 'SAD države',
+        extent: [-13884991, 2870341, -7455066, 6338219],
+        source: new ol.source.TileWMS({
+            url: 'https://ahocevar.com/geoserver/wms',
+            params: { 'LAYERS': 'topp:states', 'TILED': true },
+            serverType: 'geoserver',
+            // Countries have transparency, so do not fade tiles:
+            transition: 0
         }),
-        new ol.layer.Tile({
-            title: 'DEM',
-            source: new ol.source.TileWMS({
-                url: 'http://osgl.grf.bg.ac.rs/geoserver/osgl_2/wms',
-                params: { LAYERS: 'osgl_2:dem25', VERSION: '1.1.1' }
-            })
-        })
-    ],
+    }),
+];
+new Map({
+    layers: layers,
+    target: 'map',
     view: new ol.View({
-        center: [2279411, 5591015],
-        zoom: 7
-    })
+        center: [-10997148, 4569099],
+        zoom: 4,
+    }),
 });
